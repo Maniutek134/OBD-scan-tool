7cc50c6a5a801dece5ce5f123125be0dd2ee5792
Maciek <maciekcymbalista@gmail.com>
2017-11-09 23:54:12 +0100

    podzial na pliki

---
+++ User/bluetooth.c
@@ -0,0 +1,25 @@
+#include "bluetooth.h"
+
+char bufferRx[100];
+char bufferTx[100];
+
+
+void Bluetooth_Init(){
+	/* Initialize UART5 at 9600 baud, TX: PC12, RX: PD2 */
+	TM_USART_Init(USART, TM_USART_PinsPack_1, 9600);
+	//TM_USART_Init(UART5, TM_USART_PinsPack_1, 9600);
+}
+
+void BluetoothSend(char *buffer){
+	TM_USART_Puts(USART, buffer);
+}
+
+bool BluetoothGet(){
+	if(TM_USART_Gets(USART, bufferRx, sizeof(bufferRx))){
+		return true;
+	}
+	else{
+	return false;
+	}
+}
+
---
+++ User/bluetooth.h
@@ -0,0 +1,11 @@
+#include "tm_stm32f4_usart.h"
+#include <stdbool.h>
+
+//define proper uart, USART1-internal uart/USART5-TX: PC12, RX: PD2
+#define USART USART1
+
+
+
+void Bluetooth_Init();
+void BluetoothSend(char *buffer);
+bool BluetoothGet();
